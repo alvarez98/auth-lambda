69239a980b3c8251d35691b41d2dfda421b1c61b
Esteban Alvarez <aeap19980929@gmail.com>
2020-11-08 10:33:25 -0600

    Agregando jsdoc

--- src/utils/getFields.ts
+++ src/utils/getFields.ts
@@ -1,3 +1,10 @@
+/**
+ * @function getFields
+ * @description Obtiene los campos del evento de serverless indicados en un array
+ * @param {Object} event - Serverless event
+ * @param {Array} fields - Campos a obtener
+ * @return {String} valores obtenidos del objeto
+ */
 const getFields = (event, fields) => {
     const data = {}
     fields.forEach((item) => data[item] = event[item])
--- src/utils/verifyCredentials.ts
+++ src/utils/verifyCredentials.ts
@@ -2,6 +2,13 @@ import HttpError from "../classes/httpError"
 import getFields from "./getFields"
 import { validateApiKey } from "../db/postgres/controllers"
 
+/**
+ * @function verifyCredentials
+ * @description Verifica el header authorization
+ * @param {Array} returns - Campos a obtener
+ * @param {Object} event - Serverless event
+ * @return {Object} valores obtenidos del objeto
+ */
 const verifyCredentials = ({returns}) => async (event) => {
     const error_message = 'API KEY is required', {headers} = event   
     if (!headers.Authorization || 
